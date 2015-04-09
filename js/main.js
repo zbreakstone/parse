f5f0ad22db04775fb4cf47c8b108a0c0d08427a4
Zack Breakstone <z.breakstone@gmail.com>
2015-04-08 20:31:21 -0400

    Make basic looping word removal function

--- js/main.js
+++ js/main.js
@@ -2,22 +2,26 @@
 var p = document.getElementsByTagName('p')[0];
 var pi = p.innerHTML;
 
-var conjunctions = ["for", "and", "nor", "but", "or", "yet", "so"];
+// var conjunc = ["for", "and", "nor", "but", "or", "yet", "so"];
 
-var parse = function() {
-	var res = pi.replace(/ and /g, " ");
-	console.log(res);
-	p.innerHTML = res;
-	return res;
-};
+// var parse = function() {
+// 	var res = pi.replace(/ and /g, " ");
+// 	console.log(res);
+// 	p.innerHTML = res;
+// 	return res;
+// };
 
-var parseArray = function() {
-	var conjLength = conjunctions.length;
-	for (var i = 0; i <= conjLength; i++) {
-		new RegExp('/ ' + conjunctions[i] + ' /g');
-		var res = pi.replace(conj, " ");
-		console.log(res);
-		p.innerHTML = res;
-	};
-	return;
+var parseArray = function(text) {
+	var conjunc = ["for", "and", "nor", "but", "or", "yet", "so"];
+	for (var word in conjunc) {
+		// var re = /\s/;
+		// console.log(re);
+		console.log(word);
+		var text = text.replace(new RegExp(conjunc[word], "ig"), " ");
+		p.innerHTML = text;
+		console.log(p.innerHTML);
+	}
+	// return text;
 }
+
+// var parsed = parseArray(pi);
